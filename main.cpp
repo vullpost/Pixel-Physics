25bfb952bc30d7613c9ffc58df8c193643815cf7
vullpost <73608479+vullpost@users.noreply.github.com>
2021-03-21 22:53:00 +0100

    Fixing pointer issue as well as getting no output

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ const uint8_t specials_start = 0;
 const uint8_t solid_start = 2;
 const uint8_t fluid_start = 4;
 
-enum materials : uint8_t {
+enum materials {
   // specials
   air,  // nothing
   static_pixel,  // unbreakable
@@ -36,7 +36,7 @@ class World {
 private:
   int world_x;
   int world_y;
-  materials **world;
+  materials *world;
 public:
   World(int x, int y);
   ~World();
@@ -49,47 +49,38 @@ public:
 World::World(int x, int y) {
   world_x = x;
   world_y = y;
-  this->world = new materials*[world_y];
-  for (int i = 0; i < world_x; i++) {
-    this->world[i] = new materials[world_x];
-  }
+  this->world = new materials[world_y * world_x];
   fill_material(air);
 }
 
-/* TODO: currently doesn't seem to work because memory is freed twice here.
- * The fix is probably to make world a 1-dimensional array that is supposed
- * to be accessed by this->world[i*world_x + j] or this->world[y*world_x + x]
- */
 World::~World() {
-  for (int i = 0; i < world_y; i++) {
-    delete[] this->world[i];
-  }
   delete[] this->world;
 }
 
+// TODO: update isn't behaving as intended, the sand and dirt aren't falling
 void World::update() {
   materials current_material;
   uint8_t current_adhesion;
   for (int i = world_y - 1; i > 1; i--) {  // start from the bottom
     for (int j = 1; j < world_x - 1; j++) {
-      current_material = this->world[i][j];
+      current_material = this->world[i*world_x + j];
       if (current_material == air) {
         "";  // do nothing
       } else if (solid_start <= current_material < fluid_start) {
         current_adhesion = adhesion[current_material - solid_start];
-        if (!(solid_start <= this->world[i + 1][j] < fluid_start) &&
-        this->world[i + 1][j] != static_pixel) {
-          this->world[i][j] = this->world[i + 1][j];
-          this->world[i + 1][j] = current_material;
+        if (!(solid_start <= this->world[(i+1)*world_x + j] < fluid_start) &&
+        this->world[(i+1)*world_x + j] != static_pixel) {
+          this->world[i*world_x + j] = this->world[(i+1)*world_x + j];
+          this->world[(i+1)*world_x + j] = current_material;
         } else if (rand() % current_adhesion == 0) {
-          if (!(solid_start <= this->world[i + 1][j - 1] < fluid_start) &&
-          this->world[i + 1][j - 1] != static_pixel) {
-            this->world[i][j] = this->world[i + 1][j - 1];
-            this->world[i + 1][j - 1] = current_material;
-          } else if (!(solid_start <= this->world[i + 1][j + 1] < fluid_start) &&
-          this->world[i + 1][j + 1] != static_pixel) {
-            this->world[i][j] = this->world[i + 1][j + 1];
-            this->world[i + 1][j + 1] = current_material;
+          if (!(solid_start <= this->world[(i+1)*world_x + (j-1)] < fluid_start) &&
+          this->world[(i+1)*world_x + (j-1)] != static_pixel) {
+            this->world[i*world_x + j] = this->world[(i+1)*world_x + (j-1)];
+            this->world[(i+1)*world_x + (j-1)] = current_material;
+          } else if (!(solid_start <= this->world[(i+1)*world_x + (j+1)] < fluid_start) &&
+          this->world[(i+1)*world_x + (j+1)] != static_pixel) {
+            this->world[i*world_x + j] = this->world[(i+1)*world_x + (j+1)];
+            this->world[(i+1)*world_x + (j+1)] = current_material;
           }
         }
       } else if (fluid_start <= current_material) {
@@ -101,7 +92,7 @@ void World::update() {
 
 void World::spawn_material(materials material, int x, int y) {
   if ((0 < x < world_x) && (0 < y < world_y)) {
-    this->world[y][x] = material;
+    this->world[y*world_x + x] = material;
   } else {
     std::cout << "position out of range\n";
   }
@@ -113,11 +104,11 @@ void World::fill_material(materials material) {
     for (int j = 0; j < world_x; j++) {
       // set borders to static_pixel
       if (i == 0 || i == world_y - 1) {
-        this->world[i][j] = static_pixel;
+        this->world[i*world_x + j] = static_pixel;
       } else if (j == 0 || j == world_x - 1) {
-        this->world[i][j] = static_pixel;
+        this->world[i*world_x + j] = static_pixel;
       } else {
-        this->world[i][j] = material;
+        this->world[i*world_x + j] = material;
       }
     }
   }
@@ -126,7 +117,7 @@ void World::fill_material(materials material) {
 void World::show() {
   for (int i = 0; i < world_y; i++) {
     for (int j = 0; j < world_x; j++) {
-      std::cout << this->world[i][j];
+      std::cout << this->world[i*world_x + j];
     }
   std::cout << std::endl;
   }
